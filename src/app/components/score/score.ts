9eba415cdbc484de78d95e4387df4e81d848f0cd
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2025-09-06 13:15:24 +0200

    feat(score,input): created basic word input + display score

--- src/app/components/score/score.ts
+++ src/app/components/score/score.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-score',
   imports: [],
   templateUrl: './score.html',
-  styleUrl: './score.scss'
+  styleUrl: './score.scss',
 })
 export class Score {
-
+  @Input() foundWords: string[] = [];
+  @Input() score: number = 0;
 }
--- src/app/components/word-input/word-input.ts
+++ src/app/components/word-input/word-input.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-word-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './word-input.html',
-  styleUrl: './word-input.scss'
+  styleUrl: './word-input.scss',
 })
 export class WordInput {
+  @Output() submitWord = new EventEmitter<string>();
 
+  word!: string;
+
+  onSubmit() {
+    const trimmedWord = this.word.trim();
+    console.log('🚀 ~ WordInput ~ onSubmit ~ trimmedWord:', trimmedWord);
+    if (trimmedWord.length >= 3) {
+      this.submitWord.emit(trimmedWord);
+    } else {
+      console.log('word < 3');
+      this.word = '';
+    }
+  }
 }
