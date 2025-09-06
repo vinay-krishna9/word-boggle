a69bc9d80b8eea2dd86d2d0419f707f87fe96432
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2025-09-06 18:49:02 +0200

    feat(ui): global styles + header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Game } from './pages/game/game';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [Game],
+  imports: [Game, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
